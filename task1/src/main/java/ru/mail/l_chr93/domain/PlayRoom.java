01fa3503772d706c0a55199746363345d36c77ac
Aliaksei Chyrykau <L_chr93@mail.ru>
2021-07-30 23:01:37 +0200

    refactoring PlayRoom

--- task1/src/main/java/ru/mail/l_chr93/domain/PlayRoom.java
+++ task1/src/main/java/ru/mail/l_chr93/domain/PlayRoom.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class PlayRoom {
     private List<Toy> toyList;
     
-    public PlayRoom(List toyList) {
+    public PlayRoom(List<Toy> toyList) {
     	this.toyList = toyList;
     }
     
@@ -15,4 +15,16 @@ public class PlayRoom {
 					toy.getSize(), toy.getAgeGroup(), toy.getPrice());
 		}
 	}
+    
+    private void viewToyList(List<Toy> list) {
+		for (Toy toy : list) {
+			System.out.format("ID игрушки: %-3d название: %-7s размер: %-10s Минимальный возраст: %-3s цена: %-4d руб.%n", toy.getId(), toy.getName(),
+					toy.getSize(), toy.getAgeGroup(), toy.getPrice());
+		}
+	}
+    
+    public void filterByName(String name) {
+    	List<Toy> filterToyList = toyList.stream().filter(toy -> toy.getName().equals(name)).toList();
+    	viewToyList(filterToyList);
+    }
 }

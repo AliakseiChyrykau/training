e0ea739a5370c3d01117951d1d6864eb70e066d4
Aliaksei Chyrykau <L_chr93@mail.ru>
2021-08-01 00:38:39 +0200

    realization main method in class Run

--- task1/src/main/java/ru/mail/l_chr93/run/Run.java
+++ task1/src/main/java/ru/mail/l_chr93/run/Run.java
@@ -1,9 +1,28 @@
 package ru.mail.l_chr93.run;
 
-public class Run {
+import java.util.List;
 
+import ru.mail.l_chr93.domain.PlayRoom;
+import ru.mail.l_chr93.domain.Toy;
+import ru.mail.l_chr93.domain.size.*;
+import ru.mail.l_chr93.logic.filter.*;
+
+public class Run {
+	//private static boolean exitFlag = false;
+	
 	public static void main(String[] args) {
+		InitToyList initToyList = new InitToyList();
+		List<Toy> toyList = initToyList.getListToy();
+		PlayRoom pr = new PlayRoom(toyList, 500);
 		
+		System.out.println("Список игрушек");
+        pr.viewToyList();
+		
+        System.out.println("Cортировка игрушек по цене" );
+		pr.sortToyList(pr.PRICE_COMPORATOR);
+		
+		System.out.println("Поиск больших игрушек");
+		pr.filterList(new SizeFilter(PlayRoom.sizeMap.get("Big")));
 	}
 
 }

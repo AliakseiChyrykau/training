867a831786c5e19d64791345941b2ece2534f8b8
Aliaksei Chyrykau <L_chr93@mail.ru>
2021-08-01 00:37:02 +0200

    update methods viewToyList() and viewToyList(List<Toy> list) add fields ageGroupMap and size map in PlayRoom class

--- task1/src/main/java/ru/mail/l_chr93/domain/PlayRoom.java
+++ task1/src/main/java/ru/mail/l_chr93/domain/PlayRoom.java
@@ -1,40 +1,73 @@
 package ru.mail.l_chr93.domain;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import ru.mail.l_chr93.domain.ageGroup.AgeGroup;
+import ru.mail.l_chr93.domain.ageGroup.ForBabies;
+import ru.mail.l_chr93.domain.ageGroup.ForChildren;
+import ru.mail.l_chr93.domain.ageGroup.ForTeenagers;
+import ru.mail.l_chr93.domain.size.Average;
+import ru.mail.l_chr93.domain.size.Big;
+import ru.mail.l_chr93.domain.size.Size;
+import ru.mail.l_chr93.domain.size.Small;
+import ru.mail.l_chr93.logic.comparator.*;
+import ru.mail.l_chr93.logic.filter.Filter; 
 
 public class PlayRoom {
-    private List<Toy> toyList;
-    
-    public PlayRoom(List<Toy> toyList, int maxTotalCostToys) {
-    	this.toyList = new ArrayList<Toy>();
-    	int index = 0;
-    	int totalCost = 0;
-    	while (toyList.size() > index + 1) {
-    		if (totalCost + toyList.get(index).getPrice() <= maxTotalCostToys) {
-    			totalCost += toyList.get(index).getPrice();
-    			this.toyList.add(toyList.get(index));
-    		}
+	
+	public static final Comparator<Toy> NAME_COMPARATOR = new NameComparator();
+	public static final Comparator<Toy> SIZE_COMPARATOR = new SizeComparator();
+	public static final Comparator<Toy> AGE_GROUP_COMPARATOR = new AgeGroupComparator();
+	public static final Comparator<Toy> PRICE_COMPORATOR = new PriceComparator();
+	public static final Comparator<Toy> NAME_AND_PRICE_COMPORATOR = NAME_COMPARATOR.thenComparing(PRICE_COMPORATOR);
+	
+	private List<Toy> toyList;
+	public static Map<String, AgeGroup> ageGroupMap = new HashMap<String, AgeGroup>();
+	public static Map<String, Size> sizeMap = new HashMap<String, Size>();
+
+	{
+		ageGroupMap.put("ForBabies", new ForBabies());
+		ageGroupMap.put("ForChildren", new ForChildren());
+		ageGroupMap.put("ForTeenagers", new ForTeenagers());
+
+		sizeMap.put("Small", new Small());
+		sizeMap.put("Average", new Average());
+		sizeMap.put("Big", new Big());
+	}
+
+	public PlayRoom(List<Toy> toyList, int maxTotalCostToys) {
+		this.toyList = new ArrayList<Toy>();
+		int index = 0;
+		int totalCost = 0;
+		while (toyList.size() > index + 1) {
+			if (totalCost + toyList.get(index).getPrice() <= maxTotalCostToys) {
+				totalCost += toyList.get(index).getPrice();
+				this.toyList.add(toyList.get(index));
+			}
 			index++;
-    	}
-    }
-    
-    public void viewToyList() {
-		for (Toy toy : toyList) {
-			System.out.format("ID игрушки: %-3d название: %-7s размер: %-10s Минимальный возраст: %-3s цена: %-4d руб.%n", toy.getId(), toy.getName(),
-					toy.getSize(), toy.getAgeGroup(), toy.getPrice());
 		}
 	}
-    
-    private void viewToyList(List<Toy> list) {
-		for (Toy toy : list) {
-			System.out.format("ID игрушки: %-3d название: %-7s размер: %-10s Минимальный возраст: %-3s цена: %-4d руб.%n", toy.getId(), toy.getName(),
-					toy.getSize(), toy.getAgeGroup(), toy.getPrice());
-		}
+
+	public void viewToyList() {
+		toyList.stream().forEach(toy -> System.out.print(toy.toString()));
 	}
-    
-    public void filterByName(String name) {
-    	List<Toy> filterToyList = toyList.stream().filter(toy -> toy.getName().equals(name)).toList();
-    	viewToyList(filterToyList);
-    }
+
+	private void viewToyList(List<Toy> list) {
+		list.stream().forEach(toy -> System.out.print(toy.toString()));
+	}
+
+	public void filterList(Filter filter) {
+		List<Toy> filterToyList = toyList.stream().filter(toy -> filter.check(toy)).toList();
+		viewToyList(filterToyList);
+	}
+	
+	public void sortToyList(Comparator<Toy> comparator) {
+		List<Toy> sortList = new ArrayList<Toy>(toyList);
+		sortList.sort(comparator);
+		viewToyList(sortList);
+	}
 }

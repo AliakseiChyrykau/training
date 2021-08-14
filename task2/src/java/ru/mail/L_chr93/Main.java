ea711141320a8036f4922116ad3ed969709fc7f7
Aliaksei Chyrykau <L_chr93@mail.ru>
2021-08-15 01:02:11 +0200

    refact: refactoring class "Main"

--- task2/src/java/ru/mail/L_chr93/Main.java
+++ task2/src/java/ru/mail/L_chr93/Main.java
@@ -1,25 +1,10 @@
 package ru.mail.L_chr93;
 
-import java.util.ResourceBundle;
-
-import ru.mail.L_chr93.domain.Text;
-import ru.mail.L_chr93.logic.findPalindrome.MaxPalindromeSearcher;
-import ru.mail.L_chr93.logic.findPalindrome.Palindrome;
-import ru.mail.L_chr93.logic.textReader.TextReader;
-import ru.mail.L_chr93.logic.textReader.TxtTextReader;
+import ru.mail.L_chr93.logic.controller.Controller;
 
 public class Main {
     public static void main(String... args) {
-        ResourceBundle rb = ResourceBundle.getBundle("settings");
-        String filePath = Main.class.getResource(rb.getString("filePath")).getPath();
-        TextReader txR = new TxtTextReader(filePath);
-
-        Text text = new Text();
-        while (txR.hasNext()) {
-            text.addElement(txR.readSentence());
-        }
-
-        Palindrome palindrome = new Palindrome(new MaxPalindromeSearcher());
-        System.out.println(palindrome.findPalindrome(text));
+        Controller controller = new Controller();
+        controller.findMaxPalindrome();
     }
 }

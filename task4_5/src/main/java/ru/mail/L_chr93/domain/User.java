c98e85e15fff6057486e7a79dad643fcd2ca52f0
Aliaksei Chyrykau <L_chr93@mail.ru>
2021-10-01 23:04:57 +0200

    update pom.xml, web.xml, class User

--- task4_5/src/main/java/ru/mail/L_chr93/domain/User.java
+++ task4_5/src/main/java/ru/mail/L_chr93/domain/User.java
@@ -1,5 +1,64 @@
 package ru.mail.L_chr93.domain;
 
-public class User {
+import java.util.Objects;
 
+import javax.management.loading.PrivateClassLoader;
+
+public class User {
+	
+	private Long id = null;
+	private String name;
+	private String password;
+	private Role role;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public Role getRole() {
+		return role;
+	}
+	
+	public void setRole(Role role) {
+		this.role = role;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name, password, role);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id) && Objects.equals(name, other.name)
+				&& Objects.equals(password, other.password) && role == other.role;
+	}
+	
 }

1363f653f888d1b939821e0f446bc6b7e29fa2ed
Aliaksei Chyrykau <L_chr93@mail.ru>
2021-10-07 21:15:33 +0200

    dao

--- task4_5/src/main/java/ru/mail/L_chr93/dao/UserDao.java
+++ task4_5/src/main/java/ru/mail/L_chr93/dao/UserDao.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import ru.mail.L_chr93.domain.User;
 
-public interface userDao extends Dao<User> {
+public interface UserDao extends Dao<User> {
     
     List<User> userList() throws DaoException;
     User readByLoginAndPassword(String login, String password) throws DaoException;
